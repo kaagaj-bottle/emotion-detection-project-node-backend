e572f9e75318512b9a37fc2fffe514807bfdba46
Zishan Siddique <zishansiddique80@gmail.com>
2023-05-24 14:50:37 +0545

    model for song created

---
+++ models/index.js
@@ -0,0 +1,5 @@
+const Song = require("./song");
+
+Song.sync();
+
+module.exports = { Song };
---
+++ models/song.js
@@ -0,0 +1,35 @@
+const { Model, DataTypes } = require("sequelize");
+
+const { sequelize } = require("../utilities/db");
+
+class Song extends Model {}
+
+Song.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    url: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    emotion: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: "song",
+  }
+);
+
+module.exports=Song
